f4b0ba8e4cc567a7ab5d0959abb83de994809ec9
Pamella Bezerra <pammbsilva@gmail.com>
2019-02-05 12:13:03 -0300

    revert debug-toolbar changes

--- pokebattle/settings/base.py
+++ pokebattle/settings/base.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django_js_reverse',
     'webpack_loader',
     'import_export',
-    'debug_toolbar',
 
     'common',
     'users',
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'pokebattle.urls'
