8b66e9db225fc9e9b01a2260f336237154670229
Pamella Bezerra <pammbsilva@gmail.com>
2019-02-07 08:41:05 -0300

    update jest-setup.js import syntax

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+const enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 enzyme.configure({ adapter: new Adapter() });
