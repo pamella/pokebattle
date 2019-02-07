15652bf40bec7794c63a795901b10a3bdbd5865e
Pamella Bezerra <pammbsilva@gmail.com>
2019-02-07 10:25:55 -0300

    add polyfill to babel presets

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/polyfill'],
   plugins: ['react-hot-loader/babel'],
 };
