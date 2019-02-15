602472cfac98ae2e49b10b452f32074a8cc6aa37
Pamella Bezerra <pammbsilva@gmail.com>
2019-02-15 15:43:46 -0300

    add simple feedback for submitted battle

--- battles/views.py
+++ battles/views.py
@@ -1,18 +1,29 @@
-# from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
 
 from battles.forms import CreateBattleForm
 from battles.models import Battle
 
 
-class CreateBattleView(LoginRequiredMixin, CreateView):     # pylint: disable=too-many-ancestors
+class CreateBattleView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    CreateView
+):  # pylint: disable=too-many-ancestors
     template_name = "battles/create_battle.html"
-    success_url = "/"
     model = Battle
     form_class = CreateBattleForm
+    success_url = "/"
+    success_message = '%(trainer)s, your challenge was successfully submitted!'
 
     def get_initial(self):
         return {
             'user': self.request.user
         }
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            trainer=self.request.user,
+        )
