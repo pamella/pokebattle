3939a9e033abcbe364ec0e3037c4d2827819b5e3
Pamella Bezerra <pammbsilva@gmail.com>
2019-02-18 11:38:56 -0300

    delete commented lines

--- battles/admin.py
+++ battles/admin.py
@@ -3,10 +3,5 @@ from django.contrib import admin
 from battles.models import Battle, TrainerTeam
 
 
-# class BattleAdmin(admin.ModelAdmin):
-#     list_display = ('trainer_opponent', )
-
-
-# admin.site.register(Battle, BattleAdmin)
 admin.site.register(Battle)
 admin.site.register(TrainerTeam)
