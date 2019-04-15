fa325811ea754de0a64710014b32f4e4d58746df
Pamella Bezerra <pammbsilva@gmail.com>
2019-04-15 11:21:57 -0300

    update battle schema

--- assets/js/utils/schema.js
+++ assets/js/utils/schema.js
@@ -1,9 +1,20 @@
 import { schema } from 'normalizr';
 
-const pokemon = new schema.Entity('pokemon');
-const battle = new schema.Entity('battle');
+const pokemon = new schema.Entity('pokemons');
+const round = new schema.Entity('rounds', {
+  creator_pokemon: [pokemon],
+  opponent_pokemon: [pokemon],
+});
+const battle = new schema.Entity('battles', {
+  rounds: {
+    0: [round],
+    1: [round],
+    2: [round],
+  },
+});
+const listBattles = [battle];
 
 export default {
-  pokemon,
   battle,
+  listBattles,
 };
