94bd37b646d88776fa22b5f5b96a791a9c20d797
Pamella Bezerra <pammbsilva@gmail.com>
2019-04-17 10:45:47 -0300

    simple create battle formik form

--- assets/js/app/battles/components/CreateBattle/CreateBattle.js
+++ assets/js/app/battles/components/CreateBattle/CreateBattle.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 
-function BattleCreate() {
-  return (
-    <div>
-      initial battle create
-    </div>
-  );
-}
+const BattleCreate = withFormik({
+  mapPropsToValues: () => ({
+    trainer_opponent: '',
+    pokemon_1: '',
+    order_1: 0,
+    pokemon_2: '',
+    order_2: 1,
+    pokemon_3: '',
+    order_3: 2,
+  }),
 
+  handleSubmit: (values) => {
+    console.log(values);
+  },
+});
 
-export default BattleCreate;
+const BattleCreateForm = () => (
+  <Form>
+    <p>
+      Opponent:
+      <Field type="email" name="trainer_opponent" />
+    </p>
+    <p>
+      Pokemon:
+      <Field type="text" name="pokemon_1" />
+      Round:
+      <Field component="select" name="order_1">
+        <option value="0">First</option>
+        <option value="1">Second</option>
+        <option value="2">Third</option>
+      </Field>
+    </p>
+    <p>
+      Pokemon:
+      <Field type="text" name="pokemon_2" />
+      Round:
+      <Field component="select" name="order_2">
+        <option value="0">First</option>
+        <option value="1">Second</option>
+        <option value="2">Third</option>
+      </Field>
+    </p>
+    <p>
+      Pokemon:
+      <Field type="text" name="pokemon_3" />
+      Round:
+      <Field component="select" name="order_3">
+        <option value="0">First</option>
+        <option value="1">Second</option>
+        <option value="2">Third</option>
+      </Field>
+    </p>
+
+    <Field type="submit" value="Challenge now" />
+  </Form>
+);
+
+export default BattleCreate(BattleCreateForm);
