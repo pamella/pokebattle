cc3b6ac0d8c2f6c070e053c64e5edd778d6c01ff
Pamella Bezerra <pammbsilva@gmail.com>
2019-04-22 11:36:24 -0300

    add react-sortable-hoc to project

--- assets/js/app/battles/components/CreateBattle/CreateBattle.js
+++ assets/js/app/battles/components/CreateBattle/CreateBattle.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { withFormik, Form, Field } from 'formik';
 import actions from 'actions';
 import Urls from 'utils/urls';
 import { STATUS_201 } from '../../../../constants';
 
+
+const CreateBattleContainerStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+`;
 
 const BattleCreate = withFormik({
   mapPropsToValues: () => ({
@@ -31,44 +39,48 @@ const BattleCreateForm = (props) => {
   }
 
   return (
-    <Form>
-      <p>
-        Opponent:
-        <Field type="text" name="trainer_opponent" />
-      </p>
-      <p>
-        Pokemon:
-        <Field type="text" name="pokemon_1" />
-        Round:
-        <Field component="select" name="order_1">
-          <option value="0">First</option>
-          <option value="1">Second</option>
-          <option value="2">Third</option>
-        </Field>
-      </p>
-      <p>
-        Pokemon:
-        <Field type="text" name="pokemon_2" />
-        Round:
-        <Field component="select" name="order_2">
-          <option value="0">First</option>
-          <option value="1">Second</option>
-          <option value="2">Third</option>
-        </Field>
-      </p>
-      <p>
-        Pokemon:
-        <Field type="text" name="pokemon_3" />
-        Round:
-        <Field component="select" name="order_3">
-          <option value="0">First</option>
-          <option value="1">Second</option>
-          <option value="2">Third</option>
-        </Field>
-      </p>
+    <CreateBattleContainerStyled>
+      <h2>Select your opponent and your team to battle!</h2>
 
-      <Field type="submit" value="Challenge now" />
-    </Form>
+      <Form>
+        <p>
+          Opponent:
+          <Field type="text" name="trainer_opponent" />
+        </p>
+        <p>
+          Pokemon:
+          <Field type="text" name="pokemon_1" />
+          Round:
+          <Field component="select" name="order_1">
+            <option value="0">First</option>
+            <option value="1">Second</option>
+            <option value="2">Third</option>
+          </Field>
+        </p>
+        <p>
+          Pokemon:
+          <Field type="text" name="pokemon_2" />
+          Round:
+          <Field component="select" name="order_2">
+            <option value="0">First</option>
+            <option value="1">Second</option>
+            <option value="2">Third</option>
+          </Field>
+        </p>
+        <p>
+          Pokemon:
+          <Field type="text" name="pokemon_3" />
+          Round:
+          <Field component="select" name="order_3">
+            <option value="0">First</option>
+            <option value="1">Second</option>
+            <option value="2">Third</option>
+          </Field>
+        </p>
+
+        <Field type="submit" value="Challenge now" />
+      </Form>
+    </CreateBattleContainerStyled>
   );
 };
 
