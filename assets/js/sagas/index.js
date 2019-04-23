94cd3a563457a16d1ddf8a0a63333496491b8725
Pamella Bezerra <pammbsilva@gmail.com>
2019-04-23 09:35:38 -0300

    update rootSaga

--- assets/js/sagas/index.js
+++ assets/js/sagas/index.js
@@ -4,7 +4,9 @@ import {
   FETCH_DETAIL_BATTLE_REQUEST,
   POST_CREATE_BATTLE_REQUEST,
 } from '../constants/battle';
+import { FETCH_LIST_USER_REQUEST } from '../constants/user';
 import battleSagas from './battleSagas';
+import userSagas from './userSagas';
 
 
 export default function* rootSaga() {
@@ -12,5 +14,6 @@ export default function* rootSaga() {
     takeLatest(FETCH_LIST_BATTLE_REQUEST, battleSagas.loadListBattle),
     takeLatest(FETCH_DETAIL_BATTLE_REQUEST, battleSagas.loadDetailBattle),
     takeLatest(POST_CREATE_BATTLE_REQUEST, battleSagas.createBattle),
+    takeLatest(FETCH_LIST_USER_REQUEST, userSagas.loadListBattle),
   ]);
 }
